3ae641380ece69202fb776ce9e6d7c85c2e2a03d
zotin <zotinalexander@gmail.com>
2023-02-16 19:18:39 +0100

    commit

---
+++ src/main/java/com/example/homework13februar/Homework13FebruarApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework13februar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework13FebruarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework13FebruarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework13februar/Homework13FebruarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework13februar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework13FebruarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
